2ba936870688f51b9d327ba135367828fd883b56
etxebe <nawrocki.bartosz@gmail.com>
2016-12-22 14:26:03 +0100

    Beta version of application checking Polish Stock Exchange

---
+++ guistock.py
@@ -0,0 +1,87 @@
+import stockchecker
+import tkinter.simpledialog
+from tkinter import *
+
+
+class GUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("Stock Exchange Checker")
+
+        menubar = Menu(self.master)
+        filemenu = Menu(menubar, tearoff=0)
+        editmenu = Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Add company", command=self.getCompany)
+        filemenu.add_command(label="Delete company", command=self.deleteCompany)
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=self.master.quit)
+        editmenu.add_command(label="Refresh")
+        menubar.add_cascade(label="File", menu=filemenu)
+        menubar.add_cascade(label="Edit", menu=editmenu)
+        self.master.config(menu=menubar)
+
+        color = "green"
+        photo_file = "stock_down.png"
+        self.headline = Label(master, bg="#b3ccff", text="GPW", font="Verdana 10 bold")
+        self.headline.grid(row=0, column=0)
+        row = 1
+        for i in range(len(stockchecker.MY_COMPANIES)):
+            if stockchecker.CHANGE[i][0] == '-':
+                color = "red"
+                photo_file = "stock_down.png"
+            column = 0
+            self.company = Label(master, bg="#b3ccff", text=stockchecker.COMPANY[i], font="Verdana 10 bold", fg=color)
+            self.company.grid(row=row, column=column)
+            column += 1
+            self.price = Label(master, bg="#b3ccff", text=stockchecker.PRICE[i], font="Verdana 10 bold", fg=color)
+            self.price.grid(row=row, column=column)
+            column += 1
+            self.change = Label(master, bg="#b3ccff", text=stockchecker.CHANGE[i], font="Verdana 10 bold", fg=color)
+            self.change.grid(row=row, column=column)
+            column += 1
+            photo = PhotoImage(file=photo_file)
+            self.photo_label = Label(master, image=photo)
+            self.photo_label.photo = photo
+            self.photo_label.grid(row=row, column=column)
+            column += 1
+            self.percentage = Label(master, bg="#b3ccff", text=stockchecker.PERCENTAGE_CHANGE[i], font="Verdana 10 bold", fg=color)
+            self.percentage.grid(row=row, column=column)
+            column += 1
+            self.date = Label(master, bg="#b3ccff", text=stockchecker.DATE[i], font="Verdana 10 bold", fg=color)
+            self.date.grid(row=row, column=column)
+            row += 1
+
+    def getCompany(self):
+        add_company = tkinter.simpledialog.askstring("Add company", "Company name:", parent=self.master)
+        file_companies = open('companies.txt', 'a')
+        file_companies.write(add_company + '\n')
+        file_companies.close()
+
+    def deleteCompany(self):
+        delete_company = tkinter.simpledialog.askstring("Delete company", "Company name:", parent=self.master)
+        file_companies = open('companies.txt', 'r+')
+        file_lines = file_companies.readlines()
+        file_companies.seek(0)
+        for line in file_lines:
+            if line.strip() != delete_company:
+                file_companies.write(line)
+                # print(line.strip())
+        file_companies.truncate()
+        file_companies.close()
+
+def main():
+    root = Tk()
+    root.geometry("450x350+750+600")
+    root.configure(background="#b3ccff")
+    app = GUI(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    stockchecker.get_my_companies_info()
+    # print(stockchecker.MY_COMPANIES)
+    # print(stockchecker.COMPANY)
+    # print(stockchecker.PRICE)
+    # print(stockchecker.CHANGE)
+    # print(stockchecker.PERCENTAGE_CHANGE)
+    # print(stockchecker.DATE)
+    main()
---
+++ stockchecker.py
@@ -0,0 +1,51 @@
+import os.path
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+# variables that stores information for each company
+COMPANY = []
+PRICE = []
+CHANGE = []
+PERCENTAGE_CHANGE = []
+DATE = []
+
+# variable that stores what company shares you own
+MY_COMPANIES = []
+# MY_COMPANIES = ['PZU', 'PGE', 'GPW']
+
+
+def get_stock_info(company):
+    stock_site = "http://notowania.pb.pl/stocktable/GPWAKCJE"
+    page = urlopen(stock_site)
+    soup = BeautifulSoup(page, "html.parser")
+
+    all_tr_items = soup.find_all('tr')
+    for item in all_tr_items:
+        a_item = item.find('a')
+        if a_item is not None:
+            if a_item.find(text=True) == company:
+                COMPANY.append(a_item.find(text=True))
+                td_items = item.findAll('td')
+                PRICE.append(td_items[1].find(text=True))
+                CHANGE.append(td_items[2].find(text=True))
+                PERCENTAGE_CHANGE.append(td_items[3].find(text=True))
+                DATE.append(td_items[9].find(text=True))
+
+
+def get_my_companies_info():
+    if os.path.isfile('companies.txt'):
+        file_companies = open('companies.txt', 'r')
+        for company in file_companies:
+            if len(company.strip()) > 0:
+                MY_COMPANIES.append(company.strip())
+                get_stock_info(company.strip())
+        file_companies.close()
+
+if __name__ == '__main__':
+    get_my_companies_info()
+
+    print(COMPANY)
+    print(PRICE)
+    print(CHANGE)
+    print(PERCENTAGE_CHANGE)
+    print(DATE)
